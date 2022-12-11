576dedb35d3c62a2937c87f3776a41b84dd060ff
dimountain <gftght@rambler.ru>
2022-12-11 23:59:10 +0300

    Выполнил задание 2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,22 @@ public class Main {
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        System.out.println("Задача 2");
+        int clientDeviceYear = 2012;
+        clientOS = 0;
+        if (clientOS == 0) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        } else if (clientOS == 1) {
+            if (clientDeviceYear >= 2015) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+        }
     }
 }
