df38c7f374cabc624dfff59e61216ef026b40e51
dimountain <gftght@rambler.ru>
2022-12-13 21:14:37 +0300

    Выполнил задание 5

--- src/Main.java
+++ src/Main.java
@@ -52,5 +52,32 @@ public class Main {
          } else {
              System.out.println("Расстояние до банка превышает 100 км. К сожалению, мы не сможем осуществить доставку(");
          }
+
+        System.out.println("Задача 5");
+        int monthNumber = 11;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Это зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Это весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Это лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Это осень");
+                break;
+            default:
+                System.out.println("Месяца с таким порядковым номером не существует");
+        }
     }
 }
